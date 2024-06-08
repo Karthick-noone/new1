21416e1da01526c2f0bc9d951eb1589de03630d0
KARTHICK_NOONE <145643933+Karthick-noone@users.noreply.github.com>
2024-06-08 15:31:34 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,26 +1,14 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const pool = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  connectTimeout: 20000, // Increase the timeout value (in milliseconds)
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-});
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error("Error connecting to MySQL database:", err);
-    return;
-  }
-  console.log("Connected to MySQL database");
-
-  // You can release the connection after executing queries or use it for executing queries
-  connection.release();
 });
 
 module.exports = pool;
