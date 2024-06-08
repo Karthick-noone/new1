705bade5921b37a0e485a70c6bb91b17d535c47b
KARTHICK_NOONE <145643933+Karthick-noone@users.noreply.github.com>
2024-06-08 16:01:30 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -12,4 +12,4 @@ const connection = mysql.createConnection({
 });
 
 
-module.exports = connection;
+module.exports = pool;
