312a8a68e85bf088d313dc0e32ed8f251eb63d2d
KARTHICK_NOONE <145643933+Karthick-noone@users.noreply.github.com>
2024-06-08 15:36:34 +0530

    Update db.js

--- db.js
+++ db.js
@@ -11,4 +11,12 @@ const pool = mysql.createConnection({
   queueLimit: 0,
 });
 
+pool.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL database:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
+
 module.exports = pool;
